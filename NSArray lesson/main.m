b9761027c95b4b9a61354f1b554a083bea912449
Oliver Andrews <oltandrews@gmail.com>
2015-05-12 12:25:21 -0700

    Initial Commit

---
+++ NSArray lesson/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSArray lesson
+//
+//  Created by Oliver Andrews on 2015-05-12.
+//  Copyright (c) 2015 Oliver Andrews. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
